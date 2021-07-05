7816b4143280abae87fe071fd61b6b9345ada229
blueardour@server <blueardour@server>
2021-07-05 22:28:41 +0930

    try 16bit

--- train.sh
+++ train.sh
@@ -9,8 +9,8 @@
 
 #python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_ms_tiny_patch16_224 --batch-size 256 --data-path ../../data/imagenet --output_dir exp --ms_policy config/policy_tiny-8bit.txt
 
-python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_ms_tiny_patch16_224 --batch-size 256 --data-path ../../data/cifar100 --data-set CIFAR --input-size 224 --output_dir ./exp/deit_ms_tiny_patch16_224-cifar-bs256-baseline --num_workers 10 --ms_policy config/policy_tiny-baseline.txt
+#python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_ms_tiny_patch16_224 --batch-size 256 --data-path ../../data/cifar100 --data-set CIFAR --input-size 224 --output_dir ./exp/deit_ms_tiny_patch16_224-cifar-bs256-baseline --num_workers 10 --ms_policy config/policy_tiny-baseline.txt
 
-#python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_ms_tiny_patch16_224 --batch-size 256 --data-path ../../data/cifar100 --data-set CIFAR --input-size 224 --output_dir ./exp/ --num_workers 10 --ms_policy config/policy_tiny-16bit.txt
+python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_ms_tiny_patch16_224 --batch-size 256 --data-path ../../data/cifar100 --data-set CIFAR --input-size 224 --output_dir ./exp/deit_ms_tiny_patch16_224-cifar-bs256-FC16 --num_workers 10 --ms_policy config/policy_tiny-16bit.txt
 
 #python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --model deit_ms_tiny_patch16_224 --batch-size 256 --data-path ../../data/cifar100 --data-set CIFAR --input_size 32 --output_dir ./exp --num_workers 10 --ms_policy config/policy_tiny-8bit.txt
